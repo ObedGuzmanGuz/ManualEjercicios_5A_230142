144b330e6e6cb6e3f66b9824adf135c4c2cc8171
Obed Guzman Flores <230142@utxicotepec.edu.mx>
2025-02-25 00:17:28 -0600

    Practica_08
    Implementacion del navbar

--- ManualEjercicios_5A_230142/src/app/app.component.ts
+++ ManualEjercicios_5A_230142/src/app/app.component.ts
@@ -2,6 +2,7 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import {MatButtonModule} from '@angular/material/button';
 import { NavbarComponent } from './components/navbar/navbar.component';
+import 'flowbite';
 
 @Component({
   selector: 'app-root',

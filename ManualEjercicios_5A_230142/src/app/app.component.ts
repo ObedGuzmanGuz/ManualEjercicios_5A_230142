ca3061aee9b37978f09f64da5d501e2f6576d813
Obed Guzman Flores <230142@utxicotepec.edu.mx>
2025-02-28 12:43:48 -0600

    Angular
    Agregacion de breadcrumb,content,footer

--- ManualEjercicios_5A_230142/src/app/app.component.ts
+++ ManualEjercicios_5A_230142/src/app/app.component.ts
@@ -1,14 +1,24 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import {MatButtonModule} from '@angular/material/button';
 import { NavbarComponent } from './components/navbar/navbar.component';
-import 'flowbite';
+import { SidebarComponent } from './components/sidebar/sidebar.component';
+import { PageContentComponent } from './components/page-content/page-content.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { BreadcrumbComponent } from './components/breadcrumb/breadcrumb.component'; // Importación del Breadcrumb
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, MatButtonModule, NavbarComponent],
+  standalone: true,
+  imports: [
+    RouterOutlet,
+    NavbarComponent,
+    SidebarComponent,
+    PageContentComponent,
+    FooterComponent,
+    BreadcrumbComponent // Se agrega aquí
+  ],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrls: ['./app.component.css']
 })
 export class AppComponent {
   title = 'ManualEjercicios_5A_230142';
